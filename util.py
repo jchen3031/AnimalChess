d55ec48182ad36bb30df3d7e1c577cc47e4079f9
Jingyuan Chen <79951870+jchen3031@users.noreply.github.com>
2021-10-14 13:47:36 -0400

    Update util.py

--- util.py
+++ util.py
@@ -41,19 +41,23 @@ def eat(p1,p2):
         return True
     if p1.V == 9 and p2.V == 2:
         return False
+    # if p2.V == 1:
+    #     p1.round+=2
+    #     print('fake monkey')
+    #     return True
     else:
-        return p1.V>=p2.V
+        return p1.V>p2.V
 def domain(pos):
     if pos[0] <0 or pos[0]>6 or pos[1]<0 or pos[1]>8:
         return False
     return True
-def basicMove(x,y,b,bk,player, step = 0):
+def basicMove(x,y,b,bk,player,v=1, step = 0, unlegal = 1):
     probState = []
     c = 0
     for i in range(x,b.shape[0]):
         jg = ((bk[i][y] == 3 and player) or (bk[i][y] == 2 and not player)) and i !=x
-        if bk[i][y] == 1 or jg:
-            if abs(x-i)==1 and bk[i][y] != 1:
+        if bk[i][y] == unlegal or jg:
+            if abs(x-i)==1 and bk[i][y] != unlegal:
                 probState.append((i,y))
             break
         if b[i][y] == None:
@@ -62,14 +66,14 @@ def basicMove(x,y,b,bk,player, step = 0):
             if c>step:
                 break
         elif i!=x:
-            if b[i][y].player!=player:                    
+            if b[i][y].player!=player and b[i][y].V<v or (v==2 and b[i][y].V==9):
                 probState.append((i,y))
             break
     c = 0
     for i in range(x,-1,-1):
         jg = ((bk[i][y] == 3 and player) or (bk[i][y] == 2 and not player)) and i !=x
-        if bk[i][y] == 1 or jg:
-            if abs(x-i)==1 and bk[i][y] != 1:
+        if bk[i][y] == unlegal or jg:
+            if abs(x-i)==1 and bk[i][y] != unlegal:
                 probState.append((i,y))
             break
         if b[i][y] == None:
@@ -78,14 +82,14 @@ def basicMove(x,y,b,bk,player, step = 0):
             if c>step:
                 break
         elif i!=x:
-            if b[i][y].player!=player:         
+            if b[i][y].player!=player and b[i][y].V<v or (v==2 and b[i][y].V==9):         
                 probState.append((i,y))
             break
     c = 0
     for i in range(y,-1,-1):
         jg = ((bk[x][i] == 3 and player) or (bk[x][i] == 2 and not player)) and i !=y
-        if bk[x][i] == 1 or jg:
-            if abs(y-i)==1 and bk[x][i] != 1:
+        if bk[x][i] == unlegal or jg:
+            if abs(y-i)==1 and bk[x][i] != unlegal:
                 probState.append((x,i))
             break
         if b[x][i] == None:
@@ -94,14 +98,14 @@ def basicMove(x,y,b,bk,player, step = 0):
             if c>step:
                 break
         elif i!=y:
-            if b[x][i].player!=player:
+            if b[x][i].player!=player and b[x][i].V<v or (v==2 and b[x][i].V==9):
                 probState.append((x,i))
             break
     c = 0
     for i in range(y,b.shape[1]):
         jg = ((bk[x][i] == 3 and player) or (bk[x][i] == 2 and not player)) and i !=y
-        if bk[x][i] == 1 or jg:
-            if abs(y-i)==1 and bk[x][i] != 1:
+        if bk[x][i] == unlegal or jg:
+            if abs(y-i)==1 and bk[x][i] != unlegal:
                 probState.append((x,i))
             break
         if b[x][i] == None:
@@ -110,8 +114,52 @@ def basicMove(x,y,b,bk,player, step = 0):
             if c>step:
                 break
         elif i!=y:
-            if b[x][i].player!=player:
+            if b[x][i].player!=player and (b[x][i].V<v or (v==2 and b[x,i].V==9)):
                 probState.append((x,i))
             break
     return probState
-# print(len('猴子'))
+def passOneRound(b):
+    # x,y = b.shape
+    # for i in range(x):
+    #     for j in range(y):
+    for i in b:
+            if i !=None:
+                if i.round > 0:
+                    i.round -= 1
+                if i.sleep>0:
+                    i.sleep-=1
+    return b
+def countBoard(b, player = None):
+    # x,y = b.shape
+    count = 0
+    # for i in range(x):
+    #     for j in range(y):
+    for i in b:
+            if i !=None:
+                if player == None:
+                    count += 1
+                elif i.player == player:
+                    count += 1
+    return count
+def find(b, v, player):
+    for i in b:
+            if i !=None:
+                if i.player == player and i.V == v:
+                    return i
+    else:
+        return None
+def exist(pos,b,bk = None,river = True):
+    x,y = pos
+    if x<0 or x>=b.shape[0] or y<0 or y >=b.shape[1]:
+        return False
+    if river:
+        return bk[x,y] != 1
+    return True
+def pigEvolution(pos,b,player):
+    x,y = pos
+    for i in range(x-1,x+2):
+        for j in range(y-1,y+2):
+            if exist((i,j),b,None,False):
+                if b[i,j]!=None and b[i,j].player!=player:
+                    b[i,j] = None
+    return b
